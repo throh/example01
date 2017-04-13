6e4ddf46a99dba8232c9809884a3ee52406d7c3a
throh <throh18@gmail.com>
2017-04-13 17:49:33 +0900

    4/13(목) 수업내용 반영

---
+++ src/org/opentutorials/javatutorials/constant/ConstantDemo.java
@@ -0,0 +1,17 @@
+package org.opentutorials.javatutorials.constant;
+
+public class ConstantDemo {
+
+	public static void main(String[] args) {
+		//int a = 2.2;
+		//float a = 2.2;
+		//double a = 2.2 ;
+		//float a = 2.2F;
+		//int a = 2147483648;
+		//long a = 2147483648;
+		//long a = 2147483648L;
+		//byte b = 100; --> byte 및 short는 기본적으로 int 지원(단, 해당 type의 값범위 내에서만 ...)
+		short c = 200;
+	}
+
+}
---
+++ src/org/opentutorials/javatutorials/conversion/TypeConversion.java
@@ -0,0 +1,15 @@
+package org.opentutorials.javatutorials.conversion;
+
+public class TypeConversion {
+
+	public static void main(String[] args) {
+		int a = 3;
+		float b = 1.0F;
+		double c = a + b;
+		
+		System.out.println(c);
+
+
+	}
+
+}
