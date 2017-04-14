622c1242f4d31617427b3d2a64569526e9dab818
throh <throh18@gmail.com>
2017-04-14 17:43:31 +0900

    4/14(금) 수업내용 반영

---
+++ src/org/opentutorials/javatutorials/condition/LoginDemo2.java
@@ -0,0 +1,21 @@
+package org.opentutorials.javatutorials.condition;
+
+public class LoginDemo2 {
+
+	public static void main(String[] args) {
+		String id = args[0];
+		String password = args[1];
+		if (id.equals("egoing")) {
+			if (password.equals("111111")) {
+				System.out.println("OK");
+			} else {
+				System.out.println("wrong");
+			}
+
+		} else {
+			System.out.println("NG");
+
+		}
+
+	}
+}
