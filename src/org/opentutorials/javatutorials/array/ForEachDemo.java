a12cce5fbb4da898ebb260b8f5fb129579ba4f75
throh <throh18@gmail.com>
2017-04-18 17:52:39 +0900

    4/18(화) 수업내용 반영

---
+++ src/org/opentutorials/javatutorials/array/ForEachDemo.java
@@ -0,0 +1,15 @@
+package org.opentutorials.javatutorials.array;
+
+public class ForEachDemo {
+
+	public static void main(String[] args) {
+		
+		String[] members = { "최진혁", "최유빈", "한이람" };
+		
+		for (String throh : members) {
+			System.out.println(throh + "이 상담을 받았습니다");
+
+		}
+
+	}
+}
--- src/org/opentutorials/javatutorials/exercise/subDirList.java
+++ src/org/opentutorials/javatutorials/exercise/subDirList.java
@@ -5,8 +5,9 @@ import java.io.IOException;
 
 public class subDirList {
 
-	public static void main(String[] args) {
-		File dir = new File("c:\\");
+	public static void getFileList(String path) {
+		
+		File dir = new File(path);
 		File[] fileList = dir.listFiles();
 
 		for (int i = 0; i < fileList.length; i++) {
@@ -15,6 +16,22 @@ public class subDirList {
 				System.out.println("\t 파일이름 = " + file.getName());
 			}
 		}
+	}
+
+	public static void main(String[] args) {
+		//File dir = new File("c:\\");
+		//File[] fileList = dir.listFiles();
+
+		//for (int i = 0; i < fileList.length; i++) {
+		//	File file = fileList[i];
+		//	if (file.isFile()) {
+		//		System.out.println("\t 파일이름 = " + file.getName());
+		//	}
+		//}
+		String path  = "C:\\" ;
+		System.out.println("*** " +path + " result");
+		getFileList(path);
+		
 
 	}
 }
