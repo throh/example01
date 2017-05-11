ab548eb6a8c05c329e72870cf494341d463ad1c6
throh <throh18@gmail.com>
2017-05-11 17:51:57 +0900

    5/11(목) 수업내용 반영

---
+++ src/individual/ConditionDemo1.java
@@ -0,0 +1,21 @@
+package individual;
+
+public class ConditionDemo1 {
+
+	public static void main(String[] args) {
+		// if (false) {
+		// System.out.println("result : true");
+
+		/*
+		 * if(true) { System.out.println(1); System.out.println(2); }
+		 * System.out.println(7); }
+		 */
+
+		if (true) {
+			System.out.println(7);
+		} else {
+			System.out.println(77);
+		}
+	}
+
+}
---
+++ src/individual/EqualDemo.java
@@ -0,0 +1,23 @@
+package individual;
+
+public class EqualDemo {
+
+	public static void main(String[] args) {
+		System.out.println(1 == 2);
+		System.out.println("one" != "on e");
+		System.out.println(1 <= 5);
+
+		String a = "Hello";
+		String b = "World!";
+
+		System.out.println(a == b);
+		if(a==b) {
+			System.out.println("nics");	
+		}
+		System.out.println("Bad!!");
+		
+		//System.out.println(a.equals(b));
+
+	}
+
+}
